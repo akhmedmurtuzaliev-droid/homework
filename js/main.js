da0cadf9d92e27c3a7ff185d45ff434b5ecdc1cf
akhmedmurtuzaliev-droid <akhmedmurtuzaliev@gmail.com>
2026-06-06 15:10:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// задача 1
+const firstName = 'Akhmed';
+console.log(firstName);
+const lastName = 'Murtuzaluev'
+console.log(lastName);
+const isStudent = true;
+console.log(isStudent);
+// задача 2
+const age = 22;
+console.log(age);
+const currentYear = 2026;
+console.log(currentYear)
+const birtYear = currentYear - age;
+console.log(birtYear);
+// задача 3
+const info = `меня зовут ${firstName} ${lastName} мне ${age} лет. Я ученик курса: ${isStudent}`;
+console.log(info);
+// задача 4
+let result = `123456789falsetrue`;
+console.log(result);
